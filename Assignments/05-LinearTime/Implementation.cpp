592fc3c4ab8ed17c46de1f9ea9424104c62e80d1
sgilliland <sarah13grace@gmail.com>
2020-02-18 20:49:58 -0600

    included other files
    Signed-off-by: sgilliland <sarah13grace@gmail.com>

--- Assignments/05-LinearTime/Implementation.cpp
+++ Assignments/05-LinearTime/Implementation.cpp
@@ -1,9 +1,13 @@
-
-
+// Sarah Gilliland
 #include <iostream>
 #include <string>
 #include <fstream>
 #include "List.h"
+#include "JsonFacade.hpp"       
+#include <time.h>
+#include <chrono> 
+#include "Timer.hpp" 
+#include "mygetch.hpp"
 
 using namespace std;
 
@@ -13,12 +17,13 @@ int main()
 
   W = new Dictionary;
 
-  ifstream fin("dictionary.txt");
+  ifstream fin("dict_w_defs.json");
   string word;
 
-  // while(fin>>word){
-  //   W.Add(word);
-  // }
+   while(fin>>word)
+   {
+     W.Add(word);
+   }
 
   //W.Add("Ant");
   W->Add("Dog");
