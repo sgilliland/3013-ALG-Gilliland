e6dcfb9db2da34aa0774960ece4937ebb9f905c1
sgilliland <sarah13grace@gmail.com>
2020-02-18 22:38:37 -0600

    Updated implementation code
    Signed-off-by: sgilliland <sarah13grace@gmail.com>

--- Assignments/05-LinearTime/Implementation.cpp
+++ Assignments/05-LinearTime/Implementation.cpp
@@ -14,9 +14,9 @@ using namespace std;
 int main()
 {
   // Start the timer to time loading process
-  Timer T;        // creating an object for the timer
-  T.Start();      // starting timer to time the load
-  T.Sleep(1500);  //milliseconds
+  Timer Loading;        // creating an object for the timer
+  Loading.Start();      // starting timer to time the load
+  Loading.Sleep(1500);  //milliseconds
 
   // create a list to hold the dictionary in
   Dictionary *W;  
@@ -24,43 +24,57 @@ int main()
 
   // read dictionary and load the dictionary list
   ifstream fin("test.json");      // opening json file to read from
-  string word;                    // variable to store words read from json file
+  string word;                    // variable to store words read from json
 
   while(fin>>word)                // while file is not empty
   {
     W->Add(word);                 // add the word to the list
   }
-  
-  // stop timer for loading - maybe display to screen to check?
 
+  // stop timer for loading - maybe display to screen to check?
+  Loading.End();                        // end timer
+  double l = Loading.Seconds();         // save seconds timed as the variable l
+  cout << l << " seconds" <<endl;       // just to test the timer
+  // read typed characters and put them into a vector to read as a string
+  vector<char> input;
+  char letter;                          // variable for letter read in
+  int index = 0;                        // creating an index for the vector
+  while (letter = getch() != ' '){      // loop to read until they press space
+    input[index] = letter;              // placing the new character in the vector
+    index++;                            // incrementing the index
+  }
+
+  int numSuggestions = 0;               // variable for total number of suggestions
+  string suggestion;
   // Start loop to loop 10 times
   for (int i = 0; i < 10; i++)
   {
-    // do all of the things in here
-    // start timer for searching ? or after reading characters?
-    // read typed characters using getch
-    // place typed characters into an empty string (maybe overload the + operator)
-    // maybe start timer here?
+    // start timer for searching dictionary for suggestions
+    Timer Searching;        // creating an object for the timer
+    Searching.Start();      // starting timer to time the load
+    Searching.Sleep(1500);  //milliseconds
+
     // search for suggestions
+    suggestion = "";
     // increment # of suggestions
+    numSuggestions++;
     // stop timer and display time taken to search
+    Searching.End();                        // end timer
+    double s = Searching.Seconds();         // save number of seconds as the variable s
+
     // display the ith suggestion
-    // loop back to beginning
+    cout << suggestion << '\t';             // ???????
+    
   }
     
   
   // What is this ///////////////////////////////////////////////
-  W->Print();
-  //
-  W->ReOrder();
-  //
-  W->Print();
-  //
   delete W;
   ///////////////////////////////////////////////////////////////
 
   // display number of total suggestions
-
+  cout << "\n\nThere were " << numSuggestions << " in total.";
+  
   system("pause");
   return 0;
 }
