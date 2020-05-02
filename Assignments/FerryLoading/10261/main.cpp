9d763594e1a6a056eb605854f673b05601e5313f
sgilliland <sarah13grace@gmail.com>
2020-05-01 20:55:56 -0500

    Updated code
    Signed-off-by: sgilliland <sarah13grace@gmail.com>

--- Assignments/FerryLoading/10261/main.cpp
+++ Assignments/FerryLoading/10261/main.cpp
@@ -1,98 +1,134 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Author:           Sarah Gilliland
+// Email:            sarah13grace@gmail.com
+// Label:            10261
+// Title:            FerryLoading I
+// Course:           3013
+// Semester:         Spring 2020
+// File Name:        main.cpp
+//
+// Description:
+//  This program reads in information about a ferry which transports up to a
+//  certain amount of cars in a certain amount of time. The program uses a
+//  and arrays to keep track of which side a car is on, when it arrived, and
+//  when it reached the other side - all while maintaining the original order
+//  from the input.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
 #include <iostream>
-#include <string>
 #include <queue>
+#include <string>
 
 using namespace std;
 
-bool checkLoad(queue <int> side, int clock)
+// Function to fill the queue (bank) and the array (order)
+void Fill(queue<int> bank[], int *order, int arrival, string side, int m)
 {
-  bool canLoad = false;
-  if (side.front() <= clock)
-    canLoad = true;
-  return canLoad;
-}
-int Load(queue <int> side, int capacity, int carsSoFar)
-{
-  if (carsSoFar <= capacity){
-    side.pop();
-    carsSoFar++;
-  }
-  return carsSoFar;
+    // Go through the data and fill the queue and array
+    for (int i = 0; i < m; i++)
+    {
+        // Read in a new car
+        cin >> arrival >> side;
+
+        // Load the respective side of the queue with index of order array
+        if (side == "left")
+            bank[0].push(i);
+        else if (side == "right")
+            bank[1].push(i);
+
+        order[i] = arrival; // load arrival times in the order from the file
+    }
 }
 
-void UnLoad(queue <int> side, int numCars, int clock){
-  for (int i = 0; i < numCars; i++){
-    cout << clock << '\n'; 
-  }
+// Function to display the output in the correct order
+void display(int *results, int m)
+{
+    for (int i = 0; i < m; i++)
+        cout << results[i] << '\n';
 }
 
 int main()
 {
-  int n, t, m, arrival, c, numOnFerry, clock;
-  int cases = 0;
-  string side, car;
-  
-  cin >> c; // Read in number of cases
+    // Declaring variables
+    int n, t, m, arrival, C, numOnFerry, clock, newTime;
+    int cases = 0;
+    string side;
+    bool leftOrRight; // left will be false
 
-  // Queues hold int of the arrival time for each car
-  queue <int> Left; // Left cars in left queue
-  queue <int> Right;  // Right cars in right queue
+    cin >> C; // Read in number of cases
 
-  
-  // For howerver many cases
-  while (cases < c)
-  {
-    // Read in n, t, and m
-    cin >> n >> t >> m;
-
-    // Go through the data and fill the two queues
-    for (int i = 0; i < m; i++)
+    // For every case
+    while (cases < C)
     {
-      // Read in car
-      cin >> arrival >> side;
-      // Load the respective queue
-      if (side == "left")
-        Left.push(arrival);
-      else if (side == "right")
-        Right.push(arrival);
+        // Read in n, t, and m
+        cin >> n >> t >> m;
+
+        // bank holds the index of the car on the left or right side
+        queue<int> bank[2];
+        // order holds the order of the input
+        int *order;
+        order = new int[m];
+        // Begin on the left side each time
+        leftOrRight = false;
+        // Result holds the resulting time to display
+        int *Result;
+        Result = new int[m];
+        clock = 0;
+
+        Fill(bank, order, arrival, side, m);
+
+        // while there are cars on at least one side
+        while (!bank[0].empty() || !bank[1].empty())
+        {
+            // newTime is the variable for the new time
+            newTime = 0;
+
+            if (bank[0].empty()) // Check for left side empty
+                newTime = order[bank[1].front()];
+            else if (bank[1].empty()) // Check for right side empty
+                newTime = order[bank[0].front()];
+            else // neither is empty
+            {
+                if (order[bank[0].front()] <= order[bank[1].front()]) // left arrove first
+                    newTime = order[bank[0].front()];
+                else // right arrove first
+                    newTime = order[bank[1].front()];
+            }
+
+            // if clock needs to be updated
+            if (newTime >= clock)
+                clock = newTime;
+
+            numOnFerry = 0;
+
+            // unload and reload the Ferry
+            while ((!bank[leftOrRight].empty()) && (clock >= order[bank[leftOrRight].front()]) && (numOnFerry < n))
+            {
+                // unload: store the time of unloading into Result
+                Result[bank[leftOrRight].front()] = clock + t;
+                // reload: pop from bank and increment numOnFerry
+                bank[leftOrRight].pop();
+                numOnFerry++;
+            }
+
+            clock += t; //increment clock
+
+            if (leftOrRight)         // if on right side (true)
+                leftOrRight = false; // go to left side (false)
+            else                     // if on left side (false)
+                leftOrRight = true;  // go to right side (true)
+        }
+
+        display(Result, m); // display the results
+
+        // Get rid of allocated memory
+        delete[] order;
+        delete[] Result;
+
+        cout << '\n';
+        cases++;
     }
-
-    // Starting
-    clock = 0;
-    for (int j = 0; j < m;){
-      numOnFerry = 0;
-      // Start on left side
-      // If there are any cars on left queue w/ time <= clock, load
-      if (checkLoad(Left, clock)){
-        j = Load(Left, n, numOnFerry);
-        // Cross the river
-        clock += t;
-      }
-      else{ // If there were no cars to load, just cross the river
-        //
-        clock += t;
-      }
-
-      // Now unload
-      UnLoad(Left, numOnFerry, clock);
-      numOnFerry = 0;
-
-      // Now check the right side
-      if (checkLoad(Right, clock)){
-        j = Load(Right, n, numOnFerry);
-        // Cross the river
-        clock += t;
-      }
-      else{ // If there were no cars to load, just cross the river
-        //
-        clock += t;
-      }
-
-      // Now unload
-      UnLoad(Right, numOnFerry, clock);
-    }
-    cout << '\n';
-    cases++;
-  }
-  return 0;
+    return 0;
 }
